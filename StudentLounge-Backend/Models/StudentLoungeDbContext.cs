20aa747aa6798ce3775068de23d6eee3601542f3
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-10-18 15:13:06 +0200

    Ajout EntityFrameworkCore & Identity (pas encore de migration)

---
+++ StudentLounge-Backend/Models/StudentLoungeDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentLounge_Backend.Models
+{
+    public class StudentLoungeDbContext : IdentityDbContext<StudentLoungeUser>
+    {
+
+    }
+}
