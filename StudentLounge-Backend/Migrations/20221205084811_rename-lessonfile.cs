207eaffe438cdee6cdb74544bd04f65fa497fbe4
Kamal <k.ayadi@student.helmo.be>
2022-12-05 09:57:18 +0100

    Correction LessonFile Name

---
+++ StudentLounge-Backend/Migrations/20221205084811_rename-lessonfile.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentLounge_Backend.Migrations
+{
+    public partial class renamelessonfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "LessonFiles",
+                newName: "FileName");
+
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "LessonFiles",
+                newName: "FilePath");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "LessonFiles",
+                newName: "FileName");
+
+            migrationBuilder.RenameColumn(
+                name: "Path",
+                table: "LessonFiles",
+                newName: "FilePath");
+        }
+    }
+}
