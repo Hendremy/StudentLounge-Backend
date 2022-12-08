b5b4641fd9f0ee4e02fc0e4aebb02a57592b865c
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-12-08 11:05:18 +0100

    Fix tutoringdto, infos user direct en string plutôt qu'objet

--- StudentLounge-Backend/Models/DTOs/TutoringDTO.cs
+++ StudentLounge-Backend/Models/DTOs/TutoringDTO.cs
@@ -5,12 +5,14 @@ namespace StudentLounge_Backend.Models.DTOs
     public class TutoringDTO
     {
         public int Id { get; private set; }
-        public object User { get; private set; }
+        public string Username { get; private set; }
+        public string? Userimage { get; private set; }
 
         public TutoringDTO(Tutoring tutoring)
         {
             Id = tutoring.Id;
-            User = new { tutoring.Tutored.Fullname, tutoring.Tutored.Image };
+            Username = tutoring.Tutored.Fullname;
+            Userimage = tutoring.Tutored.Image;
         }
     }
 }
