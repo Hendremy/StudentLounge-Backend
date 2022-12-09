fff5edd5108048ecdcb21ced9dcb35eb67442cf7
Kamal <k.ayadi@student.helmo.be>
2022-12-09 14:48:22 +0100

    Changement sur Tutoring DTO

--- StudentLounge-Backend/Models/DTOs/TutoringDTO.cs
+++ StudentLounge-Backend/Models/DTOs/TutoringDTO.cs
@@ -7,12 +7,14 @@ namespace StudentLounge_Backend.Models.DTOs
         public int Id { get; private set; }
         public string Username { get; private set; }
         public string? Userimage { get; private set; }
+        public string? Tutorname { get; private set; }
 
         public TutoringDTO(Tutoring tutoring)
         {
             Id = tutoring.Id;
             Username = tutoring.Tutored.Fullname;
             Userimage = tutoring.Tutored.Image;
+            Tutorname = tutoring.Tutor?.Fullname;
         }
     }
 }
