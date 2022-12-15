fe9716c8ded62c89182d9a8eee828872041367c2
Kamal <k.ayadi@student.helmo.be>
2022-12-15 15:33:06 +0100

    api pour get les users

---
+++ StudentLounge-Backend/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentLounge_Backend.Models;
+using StudentLounge_Backend.Models.DTOs;
+using StudentLounge_Backend.Models.Files;
+
+namespace StudentLounge_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    public class UsersController : SecuredController
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UsersController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetUsers()
+        {
+            var users = _appDbContext.AppUsers.ToList();
+            return Ok(ConvertUserToDTO(users));
+        }
+
+        private IEnumerable<UserDTO> ConvertUserToDTO(IEnumerable<AppUser> users)
+        {
+            IList<UserDTO> usersDTO = new List<UserDTO>();
+            foreach (var user in users)
+            {
+                usersDTO.Add(new UserDTO(user));
+            }
+            return usersDTO;
+        }
+
+    }
+}
---
+++ StudentLounge-Backend/Models/DTOs/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using StudentLounge_Backend.Models.Tutorats;
+
+namespace StudentLounge_Backend.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; private set; }
+        public string Fullname { get; private set; }
+        public string? Image { get; private set; }
+
+        public UserDTO(AppUser user)
+        {
+            Id = user.Id;
+            Fullname = user.Fullname;
+            Image = user.Image;
+        }
+    }
+}
