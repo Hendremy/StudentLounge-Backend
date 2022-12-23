5904180d5d39c5c3f167e77f57ad7dc2c29754ee
E190449 <r.hendrice@student.helmo.com>
2022-12-23 17:25:35 +0100

    Fix url tutorings & chats controller

--- StudentLounge-Backend/Controllers/ChatsController.cs
+++ StudentLounge-Backend/Controllers/ChatsController.cs
@@ -25,10 +25,11 @@ namespace StudentLounge_Backend.Controllers
             {
                 string userId = GetUserId();
                 var tutorings = _context.Tutorings
+                    .Include(tutoring => tutoring.Tutor)
+                    .Include(tutoring => tutoring.Tutored)
                     .Where(tutoring => tutoring.Tutor != null && (tutoring.Tutored.Id == userId
                                        || tutoring.Tutor.Id == userId))
-                    .Select(tutoring => new DiscussionDTO(tutoring, userId))
-                    .ToList();
+                    .Select(tutoring => new DiscussionDTO(tutoring, userId));
 
                 return Ok(tutorings);
             }
